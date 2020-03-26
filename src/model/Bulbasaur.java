078f4ecce3d7b4d949e91105ccdbfca85fa3b81a
PRiNCE BHARDWAj <Prince>
2020-03-27 01:26:48 +0530

    FSJ0320A021

--- src/model/Bulbasaur.java
+++ src/model/Bulbasaur.java
@@ -19,5 +19,42 @@ package model;
 //evolution = "IVYSAUR"
 //baseExp = "64" 
 
-
-
+public class Bulbasaur extends Pokemon{
+	String characteristics;
+	String type;
+	String evolution;
+	String baseExp;
+	public String getCharacteristics() {
+		return characteristics;
+	}
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEvolution() {
+		return evolution;
+	}
+	public void setEvolution(String evolution) {
+		this.evolution = evolution;
+	}
+	public String getBaseExp() {
+		return baseExp;
+	}
+	public void setBaseExp(String baseExp) {
+		this.baseExp = baseExp;
+	}
+	
+	public Bulbasaur(String pokemonName, int pokemonNumber, String characteristics, String type, String evolution, String baseExp) {
+		super(pokemonName, pokemonNumber);
+		setCharacteristics(characteristics);
+		setType(type);
+		setEvolution(evolution);
+		setBaseExp(baseExp);
+	}
+	
+}
